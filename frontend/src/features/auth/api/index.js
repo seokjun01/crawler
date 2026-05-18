bb588a3443d68f82c5fff94548586cf7ace76e52
seokjun01 <suckjungod@naver.com>
2026-05-18 16:30:49 +0900

    add :회원가입 및 이메일 중복 체크 api 호출 메서드 추가

--- frontend/src/features/auth/api/index.js
+++ frontend/src/features/auth/api/index.js
@@ -5,3 +5,16 @@ import { api } from "../../../shared/api";
 export const loginApi = (email, password) => {
   return api("POST", "/api/auth/login", { email, password });
 };
+
+export const registerApi = (email, password, nickname, preferredCategories) => {
+  return api("POST", "/api/users/register", {
+    email,
+    password,
+    nickname,
+    preferred_categories: preferredCategories.join(","),
+  });
+};
+
+export const checkEmailApi = (email) => {
+  return api("GET", `/api/users/check-email?email=${email}`);
+};
