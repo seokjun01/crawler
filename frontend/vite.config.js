ccb236a90389c8112d6cc373b9d2e3aee115dc72
seokjun01 <suckjungod@naver.com>
2026-05-27 17:01:56 +0900

    fix : proxy 설정 간단하게 해도 작용

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,26 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8080",
+        target: "http://localhost:8080",
         changeOrigin: true,
-        cookieDomainRewrite: {
-          "127.0.0.1": "localhost",
-          "*": "localhost",
-        },
         secure: false,
-        configure: (proxy) => {
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            const cookies = proxyRes.headers["set-cookie"];
-            if (cookies) {
-              proxyRes.headers["set-cookie"] = cookies.map((cookie) =>
-                cookie
-                  .replace(/Domain=[^;]+;?\s*/gi, "")
-                  .replace(/SameSite=None/gi, "SameSite=Lax")
-                  .replace(/Secure;?\s*/gi, ""),
-              );
-            }
-          });
-        },
       },
     },
   },
